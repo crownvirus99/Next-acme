7addd6d1d49dd5f9ca354a56db0b4ec8d9606cab
Martin <martin.roca.99@gmail.com>
2024-02-28 11:40:44 -0300

    delete invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -53,3 +53,8 @@ const FormSchema = z.object({
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
+
+  export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+  }
